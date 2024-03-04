6ac5ba3369652c3bac1123ebde451209760c927e
se4weed <norisama1159@gmail.com>
2024-03-04 17:48:08 +0900

    05/クラスメソッドdoubleの実装

--- 01_ruby/05_idioms/idioms.rb
+++ 01_ruby/05_idioms/idioms.rb
@@ -5,3 +5,10 @@
 #   戻り値は、引数に渡した文字列を二つ連結したものを返してください e.g. "text" => "texttext"
 #   ただし、文字列 "test" を引数に渡したときに限って、次の例外を発生させてください
 #   例外は、StandardErrorを継承したDoubleErrorを発生させてください。例外クラスは、 `Idioms::` の下の名前空間で定義してください
+
+class Idioms
+  def self.double(arg)
+    raise DoubleError if arg == "test"
+    arg.to_s * 2
+  end
+end
